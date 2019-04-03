c40a127d7750789279695761b26abeef3c44aa31
Chris <chris@chris03.com>
2019-04-02 22:40:17 -0400

    Save all config in local storage; Added bus speed; Added bus stop marker; Allow to center map on bus or stop

--- bus.js
+++ bus.js
@@ -2,116 +2,260 @@ var app = angular.module('App', ['ngSanitize']);
 
 app.factory('debounce', function ($timeout) {
   return function (callback, interval) {
-   var timeout = null;
-   return function (args) {
-     $timeout.cancel(timeout);
-     timeout = $timeout(function () {
-       callback.apply(this, args);
-     }, interval);
+    var timeout = null;
+    return function (args) {
+      $timeout.cancel(timeout);
+      timeout = $timeout(function () {
+        callback.apply(this, args);
+      }, interval);
     };
   };
 });
 
 app.controller('BusController', ['$scope', '$http', '$interval', '$q', 'debounce', function ($scope, $http, $interval, $q, $debounce) {
 
-  $scope.stopId = 41220;
-  $scope.presetStopIds = [41354,41220];
-  $scope.hiddenRouteTags = '42O';
-  $scope.minutesToShow = 15;
+  $scope.config = undefined;
+  $scope.presetStopIds = [41354, 41220];
+
+  $scope.allPredictions = [];
+  $scope.vehicleLocations = [];
+
   $scope.refreshInterval = 15;
   $scope.nextRefresh = 0;
-  $scope.map = new GMaps({div: '#map'   });
-  $scope.isZoomed = false;
 
+  $scope.map = undefined;
+  $scope.mapMarkers = [];
 
-  $scope.setStopId = function(id){
-    $scope.stopId = id;
+  $scope.nextBusApiUrl = '/webservicesNextbus';
+  //$scope.nextBusApiUrl = 'http://webservices.nextbus.com/service/publicJSONFeed';
 
+  $scope.loadConfig = function () {
+    var jsonConfig = window.localStorage.getItem('BusConfig');
+
+    if (jsonConfig !== null && jsonConfig.length > 0) {
+      try {
+        $scope.config = JSON.parse(jsonConfig);
+      } catch (e) {
+        console.error(e)
+      }
+    }
+
+    if (!$scope.config) {
+      // Default config
+      $scope.config = {
+        agency: 'stl',
+        stopId: 41220,
+        minutesToShow: 5,
+        hiddenRouteTags: '42O'
+      };
+    }
+  }
+
+  $scope.saveConfig = function () {
+    window.localStorage.setItem('BusConfig', JSON.stringify($scope.config));
+  }
+
+  $scope.setStopId = function (id) {
+    $scope.config.stopId = id;
     $scope.refresh();
   }
 
-  $scope.isWithinMinutes = function(){
-    return function(p) {
-      return p && p.minutes <= $scope.minutesToShow;
-    }
+  $scope.isWithinMinutes = function (route) {
+    //return function (route) {
+    return route && route.minutes <= $scope.config.minutesToShow;
+    //}
   };
 
-  $scope.refreshNow = function(){
+  $scope.getStopLocationFromPredictions = function (predictions) {
+
+    return $q(function (resolve, reject) {
+      if (predictions && ($scope.stop == undefined || $scope.stop.stopId != $scope.config.stopId)) {
+
+        var route = predictions[0].routeTag;
+
+        $http({
+          url: $scope.nextBusApiUrl,
+          params: {
+            command: 'routeConfig',
+            a: $scope.config.agency,
+            r: route
+          }
+        }).then(function (result) {
+          $scope.stop = result.data.route.stop.find(s => {
+            return s.stopId == $scope.config.stopId;
+          });
+
+          console.log('Found stop: ', $scope.stop);
+
+          // Init map
+          $scope.mapMarkers = [];
+          $scope.map = new google.maps.Map(document.getElementById('map'), {
+            center: $scope.getStopPosition(),
+            zoom: 14,
+            streetViewControl: false,
+            mapTypeControl: false
+          });
+
+          // Add marker
+          new google.maps.Marker({
+            position: $scope.getStopPosition(),
+            map: $scope.map,
+            icon: {
+              url: "https://maps.google.com/mapfiles/ms/icons/green-dot.png"
+            }
+          });
+
+          resolve();
+        });
+      } else {
+        resolve();
+      }
+    });
+  }
+
+  $scope.refreshNow = function () {
     $scope.nextRefresh = $scope.refreshInterval;
+    $scope.loading = true;
 
-    var busUrl = '/webservicesNextbus?command=predictions&a=stl&stopId=' + $scope.stopId;
-            $http({ url: busUrl }).then(function (result) {
-              $scope.data = result.data;
-              $scope.allPredictions = [];
-              $scope.loading = true;
+    $http({
+      url: $scope.nextBusApiUrl,
+      params: {
+        command: 'predictions',
+        a: $scope.config.agency,
+        stopId: $scope.config.stopId
+      }
+    }).then(function (result) {
+      $scope.data = result.data;
+      $scope.allPredictions = [];
 
-              // Extract all predictions
-              $scope.data.predictions.forEach(function(p){
+      // Extract all predictions
+      if ($scope.data.predictions) {
 
-                // Skip routes on match
-                if($scope.hiddenRouteTags.indexOf(p.routeTag) >= 0){
-                  return;
-                }
+        // Get bus stop location
+        $scope.getStopLocationFromPredictions($scope.data.predictions).then(function () {
 
-                if(p.direction){
-                 // Fix property not an array when only 1 item
-                if(!angular.isArray(p.direction.prediction)){
-                  p.direction.prediction = [p.direction.prediction];
-                }
+          $scope.data.predictions.forEach(function (route) {
+            // Skip routes on match
+            if ($scope.config.hiddenRouteTags.indexOf(route.routeTag) >= 0) {
+              return;
+            }
 
-                p.direction.prediction.forEach(function(d){
-                  $scope.allPredictions.push({
-                    route: p.routeTitle,
-                    minutes: parseInt(d.minutes),
-                    vehicle: d.vehicle
-                  });
-                });
-                }
-                $scope.loading = false;
-              });
+            // Skip no directions
+            if (!route.direction) {
+              return;
+            }
 
-              $scope.showBusOnMap();
+            // Fix property not an array when only 1 item
+            if (!angular.isArray(route.direction.prediction)) {
+              route.direction.prediction = [route.direction.prediction];
+            }
+
+            // Extract predictions
+            $scope.allPredictions = route.direction.prediction.map(d => {
+              return {
+                route: route.routeTitle,
+                routeTag: route.routeTag,
+                minutes: parseInt(d.minutes),
+                vehicle: d.vehicle
+              };
             });
 
-  };
+          });
 
-  $scope.refresh = $debounce($scope.refreshNow, 500);
+          $scope.loading = false;
 
-  $scope.showBusOnMap = function(){
-
-      var predictions = $scope.allPredictions.filter($scope.isWithinMinutes());
-      var markers = [];
-      var promises = predictions.map(function(p){
-        return $http.get('/webservicesNextbus?command=vehicleLocation&a=stl&v='+p.vehicle);
-      });
-
-
-   $q.all(promises).then(function (ret) {
-
-      $scope.map.removeMarkers();
-
-      ret.forEach(function(v){
-        $scope.map.addMarker({
-        lat: parseFloat(v.data.vehicle.lat),
-        lng: parseFloat(v.data.vehicle.lon),
-        title: v.data.vehicle.routeTag
+          $scope.showBusOnMap();
         });
-      });
-
-      if(!$scope.isZoomed) {
-	$scope.isZoomed = true;
-        $scope.map.fitZoom();
-        console.log('Zoom: ', $scope.map.zoom);
       }
     });
   };
 
-    $interval(function(){
-      if(!$scope.loading){
+  $scope.refresh = $debounce($scope.refreshNow, 1000);
+
+  $scope.clearMapMarkers = function () {
+    $scope.mapMarkers.forEach(m => {
+      m.setMap(null);
+    });
+    $scope.mapMarkers = [];
+  }
+
+  $scope.addMapMarker = function (lat, lon, title, vehicleId) {
+    $scope.mapMarkers.push(new google.maps.Marker({
+      position: {
+        lat: lat,
+        lng: lon
+      },
+      title: title,
+      map: $scope.map,
+      vehicleId: vehicleId
+    }));
+  }
+
+  $scope.showBusOnMap = function () {
+    var predictions = $scope.allPredictions.filter($scope.isWithinMinutes);
+    var vehicleLocationPromises = predictions.map(function (route) {
+      return $http({
+        url: $scope.nextBusApiUrl,
+        params: {
+          command: 'vehicleLocation',
+          a: $scope.config.agency,
+          v: route.vehicle
+        }
+      })
+    });
+
+    // When all location loaded
+    $q.all(vehicleLocationPromises).then(function (vehicleLocations) {
+
+      $scope.clearMapMarkers();
+      $scope.vehicleLocations = [];
+
+      // Add marker for each bus
+      vehicleLocations.forEach(v => {
+        $scope.vehicleLocations.push(v.data.vehicle);
+
+        $scope.addMapMarker(
+          parseFloat(v.data.vehicle.lat),
+          parseFloat(v.data.vehicle.lon),
+          v.data.vehicle.routeTag,
+          v.data.vehicle.id);
+      });
+    });
+  }
+
+  $scope.getVehicleSpeed = function (id) {
+    var vehicle = $scope.vehicleLocations.find(v => v.id == id);
+
+    return vehicle ? vehicle.speedKmHr : '?';
+  }
+
+  $scope.getStopPosition = function () {
+    return { lat: parseFloat($scope.stop.lat), lng: parseFloat($scope.stop.lon) };
+  }
+
+  $scope.mapCenterOnStop = function () {
+    $scope.map.setCenter($scope.getStopPosition());
+  }
+
+  $scope.mapCenterOnVehicle = function (vehicleId) {
+    var marker = $scope.mapMarkers.find(m => m.vehicleId == vehicleId);
+
+    if (marker) {
+      $scope.map.setCenter(marker.getPosition());
+    }
+  }
+
+  $scope.$watch('config', $scope.saveConfig, true);
+
+  $scope.loadConfig();
+
+  $interval(function () {
+    if (!$scope.loading) {
       $scope.nextRefresh--;
-      if($scope.nextRefresh <= 0){
-        $scope.refresh();
+      if ($scope.nextRefresh <= 0) {
+        $scope.refreshNow();
       }
-      };
-    }, 1000);
+    };
+  }, 1000);
+
 }]);
